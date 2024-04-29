4634a338f1dbbd9700edc4e730fc93b171d643c9
Ismail Ahmed <130091934+iyahmed@users.noreply.github.com>
2024-04-28 17:23:12 -0700

    Eighth Commit: Made working cat.

--- BlockyAnimal.js
+++ BlockyAnimal.js
@@ -101,6 +101,9 @@ let g_backRightLegThighAngle = 0;
 let g_backRightLegPawAngle = 0;
 let g_backRightLegThighAnimation = false;
 let g_backRightLegPawAnimation = false;
+// Bottom Tail's Globals
+let g_tailAngle = 0;
+let g_tailAnimation = false;
 
 
 // Set up actions for the HTMl UI elements
@@ -128,11 +131,15 @@ function addActionsForHTMLUI() {
   document.getElementById('animationbackRightLegThighOnButton').onclick = function () { g_backRightLegThighAnimation = true; };
   document.getElementById('animationbackRightLegPawOffButton').onclick = function () { g_backRightLegPawAnimation = false; };
   document.getElementById('animationbackRightLegPawOnButton').onclick = function () { g_backRightLegPawAnimation = true; };
-
+  
+  // Tail's Button Events
+  document.getElementById('animationTailOffButton').onclick = function () { g_tailAnimation = false; };
+  document.getElementById('animationTailOnButton').onclick = function () { g_tailAnimation = true; };
+  
   // Front-Left Leg's Color Slider Events
   document.getElementById('frontLeftLegPawSlide').addEventListener('mousemove', function () { g_frontLeftLegPawAngle = this.value; renderAllShapes(); });
   document.getElementById('frontLeftLegThighSlide').addEventListener('mousemove', function () { g_frontLeftLegThighAngle = this.value; renderAllShapes(); });
-
+  
   // Front-Right Leg's Color Slider Events
   document.getElementById('frontRightLegPawSlide').addEventListener('mousemove', function () { g_frontRightLegPawAngle = this.value; renderAllShapes(); });
   document.getElementById('frontRightLegThighSlide').addEventListener('mousemove', function () { g_frontRightLegThighAngle = this.value; renderAllShapes(); });
@@ -144,6 +151,9 @@ function addActionsForHTMLUI() {
   // Back-Right Leg's Color Slider Events
   document.getElementById('backRightLegPawSlide').addEventListener('mousemove', function () { g_backRightLegPawAngle = this.value; renderAllShapes(); });
   document.getElementById('backRightLegThighSlide').addEventListener('mousemove', function () { g_backRightLegThighAngle = this.value; renderAllShapes(); });
+  
+  // Tail's Color Slider Events
+  document.getElementById('tailSlide').addEventListener('mousemove', function () { g_TailAngle = this.value; renderAllShapes(); });
 
   // Angle Slider Events
   document.getElementById('angleSlide').addEventListener('mousemove', function () { g_globalAngle = this.value; renderAllShapes(); });
@@ -216,6 +226,9 @@ function updateAnimationAngles() {
   if (g_backRightLegPawAnimation === true) {
     g_backRightLegPawAngle = (30 * Math.cos(3 * g_seconds));
   }
+  if (g_tailAnimation === true) {
+    g_tailAngle = (60 * Math.cos(g_seconds)); // Double the speed of walking with a leg
+  }
 }
 
 
@@ -236,7 +249,7 @@ function renderAllShapes() {
 
   // Draw the body cube
   var body = new Cube(); // Creating the body as a large rectangle
-  body.color = [1, 1, 1, 1]; // Coloring the body white
+  body.color = [0.752, 0.752, 0.752, 1]; // Coloring the body silver
   body.matrix.translate(-0.25, -0.025, 0.0); // X and Y placements for the body
   body.matrix.rotate(-5, 1, 0, 0); // Set rotation for the body
   body.matrix.scale(0.7, 0.5, 0.7); // Scaling for the body
@@ -244,7 +257,7 @@ function renderAllShapes() {
 
   // Draw a front-left leg's thigh
   var frontLeftLegThigh = new Cube(); // Creating the front-left leg's thigh as a small rectangle
-  frontLeftLegThigh.color = [1, 1, 1, 1]; // Coloring it white
+  frontLeftLegThigh.color = [0.662, 0.662, 0.662, 1]; // Coloring it dark gray
   frontLeftLegThigh.matrix.setTranslate(-0.12, 0, 0.0); // X placement for the whole front-left leg
   frontLeftLegThigh.matrix.rotate(-5, 1, 0, 0); // Rotation for the whole front-left leg
   frontLeftLegThigh.matrix.rotate(-g_frontLeftLegThighAngle, 0, 0); // Setting the animation rotation for the whole front-left leg
@@ -261,12 +274,12 @@ function renderAllShapes() {
   frontLeftLegPaw.matrix.translate(0, 0.65, 0); // Setting the custom Y placement for the front-left leg's paw
   frontLeftLegPaw.matrix.rotate(-g_frontLeftLegPawAngle, 0, 0, 1); // Setting the animation rotation for the whole front-left leg
   frontLeftLegPaw.matrix.scale(0.20, 0.25, 0.20); // Setting the custom 1/5th, 1/4th, 1/5th scale for the front-left leg's paw
-  frontLeftLegPaw.matrix.translate(-0.5, 0.45, -0.001); // Setting the custom X, Y, and Z (to avoid x-buffering) placement for the front-left leg's paw
+  frontLeftLegPaw.matrix.translate(-0.5, 0.45, -0.001); // Setting the custom X, Y, and Z (to avoid z-buffering) placement for the front-left leg's paw
   frontLeftLegPaw.render(); // Rendering the front-left leg's paw
 
   // Draw a front-right leg's thigh
   var frontRightLegThigh = new Cube(); // Creating the front-right leg's thigh as a small rectangle
-  frontRightLegThigh.color = [1, 1, 1, 1]; // Coloring it white
+  frontRightLegThigh.color = [0.662, 0.662, 0.662, 1]; // Coloring it dark gray
   frontRightLegThigh.matrix.setTranslate(-0.12, 0, 0.5); // X placement for the whole front-right leg
   frontRightLegThigh.matrix.rotate(-5, 1, 0, 0); // Rotation for the whole left leg
   frontRightLegThigh.matrix.rotate(-g_frontRightLegThighAngle, 0, 0); // Setting the animation rotation for the whole front-right leg
@@ -288,7 +301,7 @@ function renderAllShapes() {
 
   // Draw a back-left leg's thigh
   var backLeftLegThigh = new Cube(); // Creating the back-left leg's thigh as a small rectangle
-  backLeftLegThigh.color = [1, 1, 1, 1]; // Coloring it white
+  backLeftLegThigh.color = [0.662, 0.662, 0.662, 1]; // Coloring it dark gray
   backLeftLegThigh.matrix.setTranslate(0.25, 0, 0.0); // X placement for the whole back-left leg
   backLeftLegThigh.matrix.rotate(-5, 1, 0, 0); // Rotation for the whole back-left leg
   backLeftLegThigh.matrix.rotate(-g_backLeftLegThighAngle, 0, 0); // Setting the animation rotation for the whole back-left leg
@@ -310,7 +323,7 @@ function renderAllShapes() {
 
   // Draw a back-right leg's thigh
   var backRightLegThigh = new Cube(); // Creating the back-right leg's thigh as a small rectangle
-  backRightLegThigh.color = [1, 1, 1, 1]; // Coloring it white
+  backRightLegThigh.color = [0.662, 0.662, 0.662, 1]; // Coloring it dark gray
   backRightLegThigh.matrix.setTranslate(0.25, 0, 0.5); // X placement for the whole back-right leg
   backRightLegThigh.matrix.rotate(-5, 1, 0, 0); // Rotation for the whole left leg
   backRightLegThigh.matrix.rotate(-g_backRightLegThighAngle, 0, 0); // Setting the animation rotation for the whole back-right leg
@@ -327,8 +340,33 @@ function renderAllShapes() {
   backRightLegPaw.matrix.translate(0, 0.65, 0); // Setting the custom Y placement for the back-right leg's paw
   backRightLegPaw.matrix.rotate(-g_backRightLegPawAngle, 0, 0, 1); // Setting the animation rotation for the whole back-right leg
   backRightLegPaw.matrix.scale(0.20, 0.25, 0.20); // Setting the custom 1/5th, 1/4th, 1/5th scale for the back-right leg's paw
-  backRightLegPaw.matrix.translate(-0.5, 0.45, -0.001); // Setting the custom X, Y, and Z (to avoid x-buffering) placement for the back-right leg's paw
+  backRightLegPaw.matrix.translate(-0.5, 0.45, -0.001); // Setting the custom X, Y, and Z (to avoid z-buffering) placement for the back-right leg's paw
   backRightLegPaw.render(); // Rendering the back-right leg's paw
+
+  // Draw a crown
+  var crown = new Prism();
+  crown.color = [1, 0.843, 0, 1]; // Coloring the crown gold
+  crown.matrix.translate(-0.5, 0.75, 0.15); // Setting the crown's placement on the top of the head
+  crown.matrix.scale(0.5, 0.25, 0.25); // Scaling the crown
+  crown.render(); // Rendering the crown
+
+  // Draw a head
+  var head = new Cube();
+  head.color = [0.827, 0.827, 0.827, 1]; // Coloring the head light gray
+  head.matrix.translate(-0.5, 0.5, 0.15); // Setting the head's placement at the top of the body
+  head.matrix.scale(0.25, 0.25, 0.25); // Scaling the crown
+  head.render();
+
+  // Draw a tail
+  var tail = new Cube(); // Creating the bottom tail as a small rectangle
+  tail.color = [0.662, 0.662, 0.662, 1]; // Coloring it dark gray
+  tail.matrix.translate(0.45, 0.5, 0.15); // X and Z placement for the tail
+  tail.matrix.rotate(-0.75, 1, 0, 0); // Rotation for the tail
+  tail.matrix.rotate(-g_tailAngle, 0, 0); // Setting the animation rotation for the tail
+  tail.matrix.scale(0.2, 0.4, 0.2); // Flipping the tail vertically and scaling it by 1/2th
+  /*tail.matrix.scale(0.25, 0.75, 0.25); // Setting the custom 1/4th, 3/4th, 1/4th scale for the tail
+  tail.matrix.translate(-0.5, 0, 0); // Setting the custom X placement for the bottom tail*/
+  tail.render(); // Rendering the bottom tail
 
   // Check the time at the end of the function, and show on web page
   var duration = performance.now() - startTime;
